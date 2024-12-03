35e450068396e1d6d5bf1b6ab690045434324af3
mik510 <prupletea@outlook.com>
2024-12-03 14:59:33 +0000

    wrote constructors, partially implemented parse query

--- .devcontainer/src/Lib2.hs
+++ .devcontainer/src/Lib2.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
 module Lib2
     ( Query(..),
     parseQuery,
@@ -7,25 +9,73 @@ module Lib2
     stateTransition
     ) where
 
--- | An entity which represets user input.
--- It should match the grammar from Laboratory work #1.
--- Currently it has no constructors but you can introduce
--- as many as needed.
-data Query
+import qualified Data.Char as C
+import qualified Data.List as L
 
--- | The instances are needed basically for tests
+-- <root> ::= <commands>
+-- <command> ::= <addHotelStay> | <removeHotelStay> | <changeHotelStay>
+-- <addHotelStay> ::= add <hotelStays>
+-- <removeHotelStay> ::= remove <hotelStays>
+-- <changeHotelStay> ::= change <hotelStays><hotelStays>
+
+-- <locations> ::= "Vilnius, Lithuania" | "Warsaw, Poland" | "Prague, Czechia" | "Vienna, Austria" | "Berlin, Germany"
+-- <nights> ::= 1 | 2 | 3 | 4 | 5 | 6 | 7 
+-- <hotelStays> ::= <locations><nights>
+-- <routes> ::= <hotelStays> | <hotelStays><routes>
+
+data Query =
+  LocationQuery String
+  | NightQuery Int
+  | HotelStayQuery (String, Int)
+  | RouteQuery [Query]
+
+data Location = Vilnius | Warsaw | Prague 
+  | Vienna | Berlin deriving (Eq, Show)
+data Nights = Nights Int deriving (Eq, Show)
+data HotelStay = HotelStay Location Nights deriving (Show)
+data Route = SingleStay HotelStay HotelStay | MultipleStays HotelStay Route deriving (Show)
+ 
 instance Eq Query where
-  (==) _ _= False
+  (==) :: Query -> Query -> Bool
+  (==) (LocationQuery l1) (LocationQuery l2) = l1 == l2
+  (==) (NightQuery n1) (NightQuery n2) = n1 == n2
+  (==) (HotelStayQuery (l1, n1)) (HotelStayQuery (l2, n2)) = l1 == l2 && n1 == n2
+  (==) _ _ = False
 
 instance Show Query where
-  show _ = ""
+  show :: Query -> String
+  show (LocationQuery location) = "Location: " ++ location
+  show (NightQuery nights) = "Nights: " ++ show nights
+  show (HotelStayQuery (location, nights)) = "Hotel stay: " ++ location ++ " for " ++ show nights
+  show (RouteQuery route) = "Route: " ++ show (showHotelStays route)
 
 -- | Parses user's input.
 -- The function must have tests.
 parseQuery :: String -> Either String Query
-parseQuery _ = Left "Some error message"
+parseQuery input =
+  case read input :: Maybe (String, Int) of
+    Just(location, nights)
+      | 1 <= nights && nights <= 7 -> Right $ HotelStayQuery (location, nights)
+      | otherwise -> Left "Invalid number of nights"
+    Nothing ->
+      case parseLocationQuery input of
+        Just location -> Right $ LocationQuery (show location)
+        Nothing ->
+          case parseNightsQuery input of
+            Just (Nights n) -> Right $ NightQuery n
+            Nothing ->
+              case parseRouteQuery input of
+                Just route -> Right $ RouteQuery (rotateToQueries route)
+                Nothing -> Left "Invalid input format"
 
---type Parser a = String -> a
+-- TODO implement these:
+parseLocationQuery :: String -> Maybe Location
+parseNightsQuery :: String -> Maybe Nights
+parseHotelStayQuery :: String -> Maybe HotelStay
+parseRouteQuery :: String -> Maybe Route
+
+
+type Parser a = String -> Either String (a, String)
 
 
 -- | An entity which represents your program's state.
