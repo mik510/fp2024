c1383956ea78c385b46d36738ca20ad60697598e
mik510 <prupletea@outlook.com>
2024-12-03 14:17:25 +0000

    test file back in test folder

---
+++ .devcontainer/test/Spec.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+import Test.Tasty ( TestTree, defaultMain, testGroup )
+import Test.Tasty.HUnit ( testCase, (@?=) )
+
+import Lib1 qualified
+import Lib2 qualified
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [unitTests]
+
+unitTests :: TestTree
+unitTests = testGroup "Lib1 tests"
+  [ testCase "List of completions is not empty" $
+      null Lib1.completions @?= False,
+    testCase "Parsing case 1 - give a better name" $
+      Lib2.parseQuery "" @?= Left "Some error message",
+    testCase "Parsing case 2 - give a better name" $
+      Lib2.parseQuery "o" @?= Left "Some error message"
+  ]
