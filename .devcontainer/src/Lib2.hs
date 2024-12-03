cfbc1b2e01d52f53a206083556dd915447073bd0
mik510 <prupletea@outlook.com>
2024-12-03 14:17:16 +0000

    state ADT has a constructor, emptyState is implemented, stateTransition is implemented

--- .devcontainer/src/Lib2.hs
+++ .devcontainer/src/Lib2.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Unused LANGUAGE pragma" #-}
 module Lib2
     ( Query(..),
     parseQuery,
@@ -6,6 +9,8 @@ module Lib2
     emptyState,
     stateTransition
     ) where
+import Text.Parsec ()
+-- ^ removed State(stateInput) from brackets because of ambiguity
 
 data Query =
   LocationQuery String
@@ -33,20 +38,25 @@ showHotelStays (_ : rest) = showHotelStays rest
 parseQuery :: String -> Either String Query
 parseQuery _ = Left "Some error message"
 
--- | An entity which represents your program's state.
--- Currently it has no constructors but you can introduce
--- as many as needed.
-data State
+data State = State
+  { counter :: Int
+  , message :: String
+  , location :: Maybe String
+  , nights :: Maybe Int
+  , hotelStay :: Maybe (String, Int)
+  , route :: Maybe [Query]
+  } deriving Show
 
--- | Creates an initial program's state.
--- It is called once when the program starts.
 emptyState :: State
-emptyState = error "Not implemented 1"
+emptyState = State 0 "" Nothing Nothing Nothing Nothing
 
--- | Updates a state according to a query.
--- This allows your program to share the state
--- between repl iterations.
--- Right contains an optional message to print and
--- an updated program's state.
 stateTransition :: State -> Query -> Either String (Maybe String, State)
-stateTransition _ _ = Left "Not implemented 3"
+stateTransition state query =
+  case query of
+    LocationQuery location -> Right (Just "Location set.", State (counter state) (message state) (Just location) (nights state) (hotelStay state) (route state))
+    NightQuery nights -> Right (Just "Nights set.", State (counter state) (message state) (location state) (Just nights) (hotelStay state) (route state))
+    HotelStayQuery (location', nights') ->
+      Right (Just "Hotel stay set.", State (counter state) (message state) (location state) (nights state) (Just (location', nights')) (route state))
+    RouteQuery route ->
+      Right (Just "Route set.", State (counter state) (message state) (location state) (nights state) (hotelStay state) (Just route))
+    _ -> Left "Unknown query"
