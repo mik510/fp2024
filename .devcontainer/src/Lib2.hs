b23dfcb3ca7fd77cd13b7a31bd455a9563f2745a
mik510 <prupletea@outlook.com>
2024-12-03 14:14:03 +0000

    added comment block with example parser

--- .devcontainer/src/Lib2.hs
+++ .devcontainer/src/Lib2.hs
@@ -45,3 +45,28 @@ emptyState = error "Not implemented 1"
 -- an updated program's state.
 stateTransition :: State -> Query -> Either String (Maybe String, State)
 stateTransition _ _ = Left "Not implemented 3"
+
+{-
+parseP5 s =
+  case matchHeader (L8.pack "P5") s of
+    Nothing -> Nothing
+    Just s1 ->
+      case getNat s1 of
+        Nothing -> Nothing
+        Just (width, s2) ->
+          case getNat (L8.dropWhile isSpace s2) of
+            Nothing -> Nothing
+            Just (height, s3) ->
+              case getNat (L8.dropWhile isSpace s3) of
+                Nothing -> Nothing
+                Just (maxGrey, s4)
+                  | maxGrey > 255 -> Nothing
+                  | otherwise ->
+                      case getBytes 1 s4 of
+                        Nothing -> Nothing
+                        Just (_, s5) ->
+                          case getBytes (width * height) s5 of
+                            Nothing -> Nothing
+                            Just (bitmap, s6) ->
+                              Just (Greymap width height maxGrey bitmap, s6)
+-}
